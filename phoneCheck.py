8779947e796a0a4630306bbbba6e6261e44f10b4
brianechavarria <brianechavarria7@gmail.com>
2026-04-07 22:02:36 -0500

    implementing object tracking

--- phoneCheck.py
+++ phoneCheck.py
@@ -1,4 +1,5 @@
 import cv2
+from engine.object_detection import ObjectDetection
 
 # Open the default camera
 cam = cv2.VideoCapture(0)
@@ -11,11 +12,17 @@ frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
+# Load object detection model
+od = ObjectDetection()
+
 while True:
     ret, frame = cam.read()
 
     # Write the frame to the output file
     out.write(frame)
+
+    # Detect objects in the frame
+    bounding_boxes, class_ids, scores = od.detect(frame)
 
     # Display the captured frame
     cv2.imshow('Camera', frame)
